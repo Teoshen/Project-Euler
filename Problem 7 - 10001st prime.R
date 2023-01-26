49928464a75c0461ce899477f55885b3a8640c8a
TRoemer208 <115428518+TRoemer208@users.noreply.github.com>
2023-01-26 14:33:59 -0700

    Add files via upload

---
+++ Problem 7 - 10001st prime.R
@@ -0,0 +1,25 @@
+#100001st prime
+#By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+#What is the 10 001st prime number?
+
+#Create a loop that checks the length of a vector. The vector consists of all prime numbers found, starting from 2. At each append, if the length is 10,001, it stops and prints the last value.
+n = 2
+Primes <- c()
+repeat{
+  if (length(Primes)==10001){
+    print(Primes[10001]);
+    break
+  }
+  else if(n == 2){
+    Primes <- append(Primes, n);
+    n <- (n+1)
+  }
+  else if (all(n %% 2:max(2,floor(sqrt(n))) != 0)){
+    Primes <-  append(Primes,n);
+    n <- (n+1)
+  }
+  else if (1 == 1){
+    n <- (n+1)
+  }
+}
+#Output is 104743
