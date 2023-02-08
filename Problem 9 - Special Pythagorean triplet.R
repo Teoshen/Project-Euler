487b8467867b2c8a624abbf5850f925b2a9f380a
TRoemer208 <115428518+TRoemer208@users.noreply.github.com>
2023-02-07 23:38:53 -0700

    Update Problem 9 - Special Pythagorean triplet.R

--- Problem 9 - Special Pythagorean triplet.R
+++ Problem 9 - Special Pythagorean triplet.R
@@ -1,3 +1,10 @@
+#A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+#a^2 + b^2 = c^2
+#For example, 3^2 + 4^2 = 9 + 16 = 25 = 52.
+#There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+#Find the product abc.
+
+
 RefList <- c(1:666)
 Combos <- (combn(RefList, 3, sum) == 1000)
 Combined <- data.frame(combn(RefList, 3)[, Combos])
